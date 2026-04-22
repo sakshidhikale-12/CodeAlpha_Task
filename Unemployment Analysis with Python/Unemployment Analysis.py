0de6c7d243a2712319c78ceb5a2ee0e8ea1cda8b
sakshidhikale-12 <sakshidhikale92@gmail.com>
2026-04-22 13:38:17 +0530

    Update Unemployment Analysis.py

--- Unemployment Analysis with Python/Unemployment Analysis.py
+++ Unemployment Analysis with Python/Unemployment Analysis.py
@@ -1,10 +1,13 @@
+# Import libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv("Unemployment in India.csv")
+
 print("First 5 rows:")
 print(df.head())
+
 df.columns = df.columns.str.strip()
 
 df.rename(columns={
@@ -13,10 +16,8 @@ df.rename(columns={
     'Estimated Labour Participation Rate (%)': 'Labour_Rate'
 }, inplace=True)
 
-
-print("\n missing values:")
+print("\nMissing values:")
 print(df.isnull().sum())
-
 
 df.dropna(inplace=True)
 
@@ -24,33 +25,47 @@ df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
 
 
 df['Month'] = df['Date'].dt.month
+df['Year'] = df['Date'].dt.year
+
 
 plt.figure(figsize=(10,5))
-plt.plot(df['Date'], df['Unemployment_Rate'])
-plt.title("unemployment trend over time")
+plt.plot(df['Date'], df['unemployment_Rate'])
+plt.title("unemployment Trend Over Time")
 plt.xlabel("Date")
-plt.ylabel("unemployment rate")
-plt.xticks(rotation=45)
-plt.show()
+plt.ylabel("Unemployment Rate (%)")
 
 
 plt.figure(figsize=(12,5))
 sns.barplot(x='Region', y='Unemployment_Rate', data=df)
 plt.title("unemployment by state")
 plt.xticks(rotation=90)
-plt.show()
 
 
-covid_df = df[df['Date'].dt.year == 2020]
+covid_df = df[df['Year'] == 2020]
+
+print("\nCOVID Data:")
+print(covid_df.head())
 
 plt.figure(figsize=(10,5))
 plt.plot(covid_df['Date'], covid_df['Unemployment_Rate'])
-plt.title("COVID impact on unemployment 2020")
-plt.xticks(rotation=45)
-plt.show()
+plt.title("COVID-19 Impact on Unemployment (2020)")
+plt.xlabel("Date")
+plt.ylabel("Unemployment Rate (%)")
+
+
+monthly_avg = df.groupby('Month')['Unemployment_Rate'].mean()
+
+plt.figure(figsize=(8,4))
+monthly_avg.plot(kind='bar')
+plt.title("Monthly Average Unemployment Rate")
+plt.xlabel("Month")
+plt.ylabel("Avg Rate")
 
 
 plt.figure(figsize=(6,4))
-sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
-plt.title("Correlation btween Features")
+sns.heatmap(df[['Unemployment_Rate','Employed','Labour_Rate']].corr(),
+            annot=True, cmap='coolwarm')
+plt.title("Correlation Between Features")
+
+plt.tight_layout()
 plt.show()
